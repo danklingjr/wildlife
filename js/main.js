c2c5cd3b29b9db88e33d6e376f1a296cb787e31c
Daniel Kling <dan@717studios.com>
2015-01-27 22:34:26 -0500

    Animations

--- js/main.js
+++ js/main.js
@@ -10,13 +10,40 @@ var newscroll;
 		        $( ".phantom" ).fadeIn(300);
 		
 		        up = !up;
-		        console.log(up);
+		        
 		      } else if(newscroll < 300 && up) {
 		        $( ".phantom" ).fadeOut(150);
 		        up = !up;
-		      };
+		      }
 		      mypos = newscroll;
-		      return false
+		      return false;
 
 		  
 		});
+
+$(document).ready(function() {    
+    /* Every time the window is scrolled ... */
+    $(window).scroll( function(){    
+        /* Check the location of each desired element */
+        $('.graph__wrap').each( function(){            
+            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+
+                //Code to initialize the graph here.
+                //This initialization should probably
+                //be deferred, to ensure performance,
+                //and the graphs should be marked as
+                //initialized so we dont't init them
+                //multiple times (possibly by changing
+                //their class so .each ignores them).
+        				$('.graph__inner--taxidermy').addClass('g-tax');
+        				$('.graph__inner--scenic').addClass('g-scenic');
+        				$('.graph__inner--reproductions').addClass('g-rep');
+        				$('.graph__inner--murals').addClass('g-mural');
+            }            
+        });     
+    });    
+});
